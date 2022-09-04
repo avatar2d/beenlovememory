efd7618f18724e708601dca389035a923f500ccf
Lê Đức Thuận <87764627+YuukiCute@users.noreply.github.com>
2022-09-04 20:42:43 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const yourDate = new Date("2022-02-15T19:02:00"),
+const yourDate = new Date("2020-02-15T00:00:00"),
 music = ['tinhyeumauhong', 'anhtheday', 'anhsedonem', 'yeutrongnangmai', 'emkhacgihoa', 'nucuoiem', 'tinhca', 'anhdalacvao', 'emlahoanghon', 'thichthich', 'xinmaruocdau', 'changcanembantam', 'tinhyeukhunglong', 'phaichangemdayeu', 'seetinh', 'tuthichthichthanhthuongthuong'];
 
 document.addEventListener('DOMContentLoaded', function(){
